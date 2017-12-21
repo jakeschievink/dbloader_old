9d3383edd6abc2399dc89f70f4fa6779ce3aa428
silardg <gproduct1379@yahoo.com>
2017-12-21 17:23:30 +0000

    Moved dir.js

--- project/index.js
+++ project/index.js
@@ -1,29 +1,26 @@
 var http = require('http');								// importing http
 var fs = require('fs');									// importing file system
 
-var test_dir = require(__dirname + '/test/dir.js');					// the dir scripts TODO: create a relative path
+var test_dir = require(__dirname + '/js/dir.js');					// the dir scripts TODO: create a relative path
 
 // create the server
-http.createServer(function(request, response) {  
-	
+http.createServer(function(request, response) {
 	// if we are opening index.html
 	fs.readFile(__dirname + '/views/index.html', function (err, html) {
      		if (err) {
-        		throw err; 
+        		throw err;
      		}
 
 		// our testr path
-     		var path = "/home/pi/project_dbloader/project/"; 
+     		var path = "/home/pi/project_dbloader/project/";
 
 		// give out the whole html
-    		response.writeHeader(200, {"Content-Type": "text/html"});  
-    		response.write(html);  
-    		response.end();  
+    		response.writeHeader(200, {"Content-Type": "text/html"});
+    		response.write(html);
+    		response.end();
 
-		
 		test_dir.get_usb( function(data)  {
-			console.log(data);
-			
+
 			test_dir.create_mount_folder(function(success) {
 
 				switch(success.toString()) {
@@ -60,7 +57,7 @@ http.createServer(function(request, response) {
 					console.log("Not mounted");
 				}
 			});
-		}); 
+		});
 	});
 
 }).listen(8000);		// on port 8000
