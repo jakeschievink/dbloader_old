fa9df6143fe4089c6798a4c2d271b68a2872fbc0
Wouter Schievink <meatcomputer@protonmail.com>
2018-12-21 22:02:38 -0500

    greatly shortened get_usb

--- project/py/get_usb.py
+++ project/py/get_usb.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python
 import os
+import psutil
 import sys
 
 found = 0
 found_string = ""
 
-partitionsFile = open("/proc/partitions")
-lines = partitionsFile.readlines()[2:]					#Skips the header lines
-for line in lines:
-    words = [x.strip() for x in line.split()]
-    minorNumber = int(words[1])
-    deviceName = words[3]
-    if minorNumber % 16 == 0:
-        path = "/sys/class/block/" + deviceName
-        if os.path.islink(path):
-            if os.path.realpath(path).find("/usb") > 0:
-          	found = 1
-		found_string = "/dev/%s\n" % deviceName
-		
+partitions = psutil.disk_partitions()
 
-if found:
-	sys.stdout.write(found_string)
-else:
-	sys.stdout.write("0\n")
+valid_devices = [device for device in partitions if '/mnt' in device.mountpoint]
+
+sys.stdout.write(valid_devices[0].mountpoint)
+
+#   partitionsFile = open("/proc/partitions")
+#   lines = partitionsFile.readlines()[2:]					#Skips the header lines
+#   for line in lines:
+#       words = [x.strip() for x in line.split()]
+#       minorNumber = int(words[1])
+#       deviceName = words[3]
+#       if minorNumber % 16 == 0:
+#           path = "/sys/class/block/" + deviceName
+#           if os.path.islink(path):
+#               if os.path.realpath(path).find("/usb") > 0:
+#                   found = 1
+#                   found_string = "/dev/%s\n" % deviceName
+#                   
+
+#   if found:
+#           sys.stdout.write(found_string)
+#   else:
+#           sys.stdout.write("0\n")
